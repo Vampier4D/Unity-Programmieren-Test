1a4fc5cfb28f5d8892cd6016d63134c631beb5e1
Vampier4D <99287245+Vampier4D@users.noreply.github.com>
2023-09-04 23:25:19 +0200

    +Jump Pad, +Speed Pad, -Mattis Gehirn Masse
    Das hat mir physischen schmerz bereitet...

--- My project (3)/Assets/Scripts/Controlls.cs
+++ My project (3)/Assets/Scripts/Controlls.cs
@@ -9,10 +9,14 @@ public class SC_FPSController : MonoBehaviour
     public float walkingSpeed = 10f;
     public float runningSpeed = 15f;
     public float jumpSpeed = 8.0f;
+    private float currentjumpSpeed;
+    private float currentrunningSpeed;
+    private float currentwalkingSpeed;
     public float gravity = 20.0f;
     public Camera playerCamera;
-    public float lookSpeed = 2.0f;
+    public float lookSpeed = 1.0f;
     public float lookXLimit = 45.0f;
+
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
@@ -25,24 +29,32 @@ public class SC_FPSController : MonoBehaviour
     {
         characterController = GetComponent<CharacterController>();
 
-        // Lock cursor
+      
+       // Lock cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
+        // Check if the character is grounded
+        bool isGrounded = characterController.isGrounded;
+
         // We are grounded, so recalculate move direction based on axes
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+       
+
+            // Press Left Shift to run
+            bool isRunning = Input.GetKey(KeyCode.LeftShift);
         float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        // Jump logic
+        if (Input.GetButtonDown("Jump") && canMove && isGrounded)
         {
             moveDirection.y = jumpSpeed;
         }
@@ -51,10 +63,8 @@ public class SC_FPSController : MonoBehaviour
             moveDirection.y = movementDirectionY;
         }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        if (!characterController.isGrounded)
+        // Apply gravity
+        if (!isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
@@ -62,7 +72,6 @@ public class SC_FPSController : MonoBehaviour
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
 
-        // Player and Camera rotation
         if (canMove)
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
@@ -70,5 +79,34 @@ public class SC_FPSController : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
-    }
+    } 
+        private void OnControllerColliderHit(ControllerColliderHit hit)
+        {
+            switch (hit.gameObject.tag)
+            {
+                case "SpeedBoost":
+                    walkingSpeed = 35f;
+                    runningSpeed = 55f;
+                    break;
+                case "JumpPad":
+                    jumpSpeed = 25f;
+                    break;
+                case "Ground":
+                walkingSpeed = 10f;
+                jumpSpeed = 8f;
+                runningSpeed = 15f;
+                    break;
+
+
+
+
+            }
+        }
+
+
+
+
+    
 }
+
+
